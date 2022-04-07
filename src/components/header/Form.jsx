9e52b3ac7c9f76affdb43aff4471c3b12b207f8e
AshleyMiller79 <ipekbilir79@gmail.com>
2022-04-07 23:18:17 +0300

    dd

--- src/components/header/Form.jsx
+++ src/components/header/Form.jsx
@@ -1,9 +1,16 @@
 
 import React from 'react'
+import { Button, FoodInput, FormContainer, Select } from './HeaderStyles'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <FormContainer>
+<FoodInput type="text" placeholder="Search" />
+<Button type="submit">Search</Button>
+
+<Select name= "mealTypes" id="mealTypes"></Select>
+
+    </FormContainer>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,32 @@
 
+import axios from 'axios';
 import React from 'react'
+import Header from "../../components/header/Header";
+
+
+const APP_ID = "bfbb3efc";
+const APP_KEY = "43faeee790f26cd82b28050d3031619d";
+
+
 
 const Home = () => {
+
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
+  
+  const getData = async () => {
+       const result=         await  axios.get(url);
+console.log(result);
+
+  }
+  
   return (
-    <div>Home</div>
+    <div>
+<Header/>
+
+
+
+    </div>
   )
 }
 
