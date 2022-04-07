244000920a5ea796180f325ea84bc385478c0170
AshleyMiller79 <ipekbilir79@gmail.com>
2022-04-07 22:41:18 +0300

    cc

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,31 +1,33 @@
 
 import React, {useState} from 'react'
 import { Logo, Nav,Hamburger,Menu,MenuLink } from './NavbarStyles'
-
+// import {GiHamburgerMenu} from "react-icons/gi"
 const Navbar = () => {
   const [isOpen,setIsOpen]=useState(false);
   return (
- <Nav>
-<Logo to="/home" >
-<i>{"<Clarusway>"}</i>
-<span>recipe</span>
-</Logo>
-<Hamburger onClick={()=>setIsOpen(!isOpen)} >
-  <span/>
-  <span/>
-  <span/>
-</Hamburger>
+    <Nav>
+      <Logo  to="/home">
+        <i>{"<Clarusway>"}</i>
+        <span>recipe</span>
+      </Logo>
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
+        <span />
+        <span />
+        <span />
+        {/* <GiHamburgerMenu/> */}
+      </Hamburger>
 
-<Menu osman={isOpen}>
-<MenuLink to="/about">About</MenuLink>
-<MenuLink to="/github">Github</MenuLink>
-<MenuLink to="/">Logout</MenuLink>
-
-</Menu>
-
-
- </Nav>
-  )
+      <Menu osman={isOpen}>
+        <MenuLink onClick={() => setIsOpen(!isOpen)} to="/about">
+          About
+        </MenuLink>
+        <MenuLink to="/github">Github</MenuLink>
+        <MenuLink onClick={() => setIsOpen(!isOpen)} to="/">
+          Logout
+        </MenuLink>
+      </Menu>
+    </Nav>
+  );
 }
 
 export default Navbar
