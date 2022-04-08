bbf7adee3248e1acf222604cab1e6180df060c93
Ashley Miller <81853731+AshleyMiller79@users.noreply.github.com>
2022-04-08 11:49:09 +0300

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,8 +4,8 @@ import React, {useState} from 'react'
 import Header from "../../components/header/Header";
 
 
-const APP_ID = "bfbb3efc";
-const APP_KEY = "43faeee790f26cd82b28050d3031619d";
+const APP_ID = "id";
+const APP_KEY="KEY";
 
 
 
