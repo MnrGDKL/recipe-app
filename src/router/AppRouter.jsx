9fe142dee602355db78795a2535580c53907107f
AshleyMiller79 <ipekbilir79@gmail.com>
2022-04-09 08:50:51 +0300

    sss

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer"
 import Home from "../pages/home/Home";
@@ -9,7 +9,7 @@ import Login from "../pages/login/Login";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Navbar />
      <Routes>
 <Route path="/" element={<Login/>}/>
@@ -21,7 +21,7 @@ const AppRouter = () => {
 
      </Routes>
 <Footer/>
-    </BrowserRouter>
+    </Router>
   );
 };
 
